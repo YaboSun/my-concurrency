9f6b21504a1ee415f835d6c75d4f783160d2c463
YaboSun <yabosun@163.com>
2019-12-04 14:53:54 +0800

    unsafe concurrency test

--- src/main/java/com/ybsun/myconcurrency/ConcurrencyTest.java
+++ src/main/java/com/ybsun/myconcurrency/ConcurrencyTest.java
@@ -1,12 +1,16 @@
 package com.ybsun.myconcurrency;
 
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
+import com.ybsun.myconcurrency.annotations.NotThreadSafe;
+import lombok.extern.slf4j.Slf4j;
 
+import java.util.concurrent.*;
+
+@NotThreadSafe
+@Slf4j
 public class ConcurrencyTest {
 
     // 总用户请求数
-    private static int clientTotal = 5000;
+    private static int clientTotal = 50000;
 
     // 总并发数
     private static int threadTotal = 200;
@@ -14,10 +18,29 @@ public class ConcurrencyTest {
     // 统计最终count值
     private static int count = 0;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
+
         // 通过线程池创建来实现，这里实现方式有很多种
         ExecutorService executorService = Executors.newCachedThreadPool();
+        final Semaphore semaphore = new Semaphore(threadTotal);
+        final CountDownLatch countDownLatch = new CountDownLatch(clientTotal);
 
+        for (int i = 0; i < clientTotal; i++) {
+            executorService.execute(() -> {
+                try {
+                    semaphore.acquire();
+                    add();
+                    semaphore.release();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                countDownLatch.countDown();
+            });
+        }
+
+        countDownLatch.await();
+        System.out.println("count:" + count);
+        executorService.shutdown();
     }
 
     private static void add() {
